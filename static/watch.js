5be5c8f37e8bf8edbb910d959e7d4e599b38b221
DerZombiiie <49880817+DerZombiiie@users.noreply.github.com>
2021-03-27 10:19:05 +0100

    pushable play / cc button; videoplayer.cc(overwrite) videoplayer.pp(overwrite) (cc= cpations; pp = playpause)

--- static/watch.js
+++ static/watch.js
@@ -9,7 +9,14 @@ class videoplayer {
         this.container = container;
         this.videoid = video;
 
+        // set html
         $(this.container).html(this.html)
+
+        // clickable stuff:
+        $(".play-img").click(() => {player.pp()})
+
+        this.pp(true)
+        this.cc(false)
     }
     get html() {
         return `<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.slidesharecdn.com%2Fultimatetestpatternslides-170322155753%2F95%2Fultimate-video-engineer-16x9-test-pattern-2-638.jpg%3Fcb%3D1490198558&f=1&nofb=1" alt="" style="height: 100%; width: 100%; top:0;">
@@ -43,23 +50,40 @@ class videoplayer {
             </div>
         </div>`
     }
-    pause(v) { // 
-        if(v) {
+    pp(v) { // toggle for playpause; v = overwrite
+        if(typeof(v) == "boolean") {
             this.playing = v
-            return this.play(v)
         } else {
-            this.playing = false
-            return this.play(false)
+            this.playing = !this.playing
         }
+        if(this.playing) { 
+            $(".play-img").attr({"src": "/static/play.svg"})
+        } else {
+            $(".play-img").attr({"src": "/static/pause.svg"})
+        }
+        return this.playing
     }
-    play(v) {}
+    cc(v) { // toggle for captions; v = overwrite
+        if(typeof(v) == "boolean") {
+            this.captions = v
+        } else {
+            this.captions = !this.captions
+        }
+        if(this.captions) { 
+            $(".cc-img").attr({"src": "/static/cc-on.svg"})
+        } else {
+            $("cc-img").attr({"src": "/static/cc-off.svg"})
+        }
+        return this.captions
+    }
+
 }
 
 $(document).ready(() => {
-    continue_watching = new contwatch(recommended.getrecommended) /* still tmp */
-    menu = new sidemenu(".dark-sidemenu-left-extended", ".dimm-overlay");
+    var continue_watching = new contwatch(recommended.getrecommended) /* still tmp */
+    var menu = new sidemenu(".dark-sidemenu-left-extended", ".dimm-overlay");
     player = new videoplayer(".videoplayer-container")
-
+    
     $(".menu-toggle").click(() => menu.toggle())
 
 })
