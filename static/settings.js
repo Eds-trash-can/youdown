261551b131b905d50cbb6b054292f01fd9d41b59
DerZombiiie <49880817+DerZombiiie@users.noreply.github.com>
2021-04-09 22:26:13 +0200

    more sidemenu.js stuff Now with text entrys!

--- static/settings.js
+++ static/settings.js
@@ -1,5 +1,9 @@
 $(document).ready(() => {
+    console.log("Hi, this page is now starting the initialisation process!", )
     menu = new sidemenu(".dark-sidemenu-left-extended", ".dimm-overlay", ".dark-startmenu-left", "",
-    [{"type":"sentry","name":"Overview","link":"/settings/"},{"type":"sentry","name":"","link":"/last"},{"type":"sentry","name":"","link":"/last"},{"type":"sentry","name":"New","link":"/new"}]);
+    [{"type":"sentry","name":"Overview","link":"/settings/"},
+    {"type":"topic","text":"content"},
+    {"type":"sentry","name":"","link":"/last"},
+    {"type":"sentry","name":"New","link":"/new"}]);
     $(".menu-toggle").click(() => menu.toggle())
 })
