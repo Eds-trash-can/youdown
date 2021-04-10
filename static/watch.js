5bdb9131668da539523bf9eaf7b2feef9ff55f06
DerZombiiie <49880817+DerZombiiie@users.noreply.github.com>
2021-04-10 20:26:13 +0200

    Fixed sidemenu on /watch/ to work with new sidemenu.js changes

--- static/watch.js
+++ static/watch.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     var continue_watching = new videolist(recommended.getrecommended) /* still tmp */
-    menu = new sidemenu(".dark-sidemenu-left-extended", ".dimm-overlay", ".dark-startmenu-left", "",
+    menu = new sidemenu(".dark-sidemenu-left-extended", ".dimm-overlay", "",
     [{"type":"entry","name":"Home","icon":"/static/home.svg","link":"/"},{"type":"entry","name":"Last","icon":"/static/last.svg","link":"/last"},{"type":"entry","name":"New","icon":"/static/clock.svg","link":"/new"}]);
 player = new videoplayer(".videoplayer-container", vid, {"skipnxt": "continue_watching.parsed[0].href"})
     
