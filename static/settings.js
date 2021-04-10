cc3116d57c99845d6c7e96e3455b7f26141d2107
DerZombiiie <49880817+DerZombiiie@users.noreply.github.com>
2021-04-10 21:51:47 +0200

    Something settings

--- static/settings.js
+++ static/settings.js
@@ -40,11 +40,11 @@ class stat {
 
 $(document).ready(() => {
     console.log("Hi, this page is now starting the initialisation process!", )
-    /*settingsmenu = new sidemenu(".settingsmenu", "", "settingsstartmenu", "",
+    settingsmenu = new sidemenu(".settingsmenu", "settingsstartmenu", "",
     [{"type":"sentry","name":"Overview","link":"/settings/"},
     {"type":"topic","text":"content"},
     {"type":"sentry","name":"","link":"/last"},
-    {"type":"sentry","name":"New","link":"/new"}], true); */
+    {"type":"sentry","name":"New","link":"/new"}], true);
     menu = new sidemenu(".sidemenu", ".dimm-overlay", "",
     [{"type":"space"},{"type":"entry","name":"Home","icon":"/static/home.svg","link":"/"},{"type":"entry","name":"Last","icon":"/static/last.svg","link":"/last"},{"type":"entry","name":"New","icon":"/static/clock.svg","link":"/new"}]);
 
